a5847ecabf3ac17dab97bc80775152c276204199
Nawaf <Nawaf.jr9@hotmial.com>
2020-08-07 09:25:38 +0300

    making the server running

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
-var unirest = require("unirest");
-var axios = require('axios');
-//convert it to axios 
-var req = unirest(
-  "POST",
-  "https://google-translate1.p.rapidapi.com/language/translate/v2"
-);
+// var unirest = require("unirest");
+// var axios = require('axios');
+// //convert it to axios
+// var req = unirest(
+//   "POST",
+//   "https://google-translate1.p.rapidapi.com/language/translate/v2"
+// );
 
-req.headers({
-  "x-rapidapi-host": "google-translate1.p.rapidapi.com",
-  "x-rapidapi-key": "b0aa080e42mshef526e52a9e4f17p19e78djsn974420e17a19",
-  "accept-encoding": "application/gzip",
-  "content-type": "application/x-www-form-urlencoded",
-  useQueryString: true,
+// req.headers({
+//   "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+//   "x-rapidapi-key": "b0aa080e42mshef526e52a9e4f17p19e78djsn974420e17a19",
+//   "accept-encoding": "application/gzip",
+//   "content-type": "application/x-www-form-urlencoded",
+//   useQueryString: true,
+// });
+
+// req.form({
+//   source: "en",
+//   q: "how are you to day ? ",
+//   target: "ar",
+// });
+
+// req.end(function (res) {
+//   if (res.error) throw new Error(res.error);
+
+//   console.log(res.body.data.translations);
+// });
+
+/* ----------------------------- importing packages ----------------------------- */
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+/* ----------------------------- App Configuration ----------------------------- */
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+/* ----------------------------- App Routes ----------------------------- */
+
+app.get("/", async (req, res) => {
+  res.send("I am fine bro ").status(200);
 });
 
-req.form({
-  source: "en",
-  q: "how are you to day ? ",
-  target: "ar",
-});
+/* ----------------------------- App Listener ----------------------------- */
 
-req.end(function (res) {
-  if (res.error) throw new Error(res.error);
+const PORT = 8000;
+const HOST = "localhost";
 
-  console.log(res.body.data.translations);
-});
+app.listen(PORT,HOST);
+console.log(`server running on http://${HOST}:${PORT}`);
