0fae655a3ab40b5a48c64d0e31141e179c8e4591
Nawaf <Nawaf.jr9@hotmial.com>
2020-08-07 21:59:59 +0300

    ff

--- src/routes/translateRoute.js
+++ src/routes/translateRoute.js
@@ -3,6 +3,9 @@ const express = require("express");
 const translateRouter = express.Router();
 const prepareRequest = require("../controllers/prepareRequest");
 const unirest = require("unirest");
+const translate = require("translate");
+translate.engine = 'google';
+translate.key = process.env.API_KEY;
 
 /* ----------------------------- Translate Routes ----------------------------- */
 
@@ -11,29 +14,35 @@ translateRouter.get("/", async (req, res) => {
 });
 
 //route
-translateRouter.post("/", async (req, res) => {
-  try {
-    const body = req.body;
-    const request = await prepareRequest(body);
-    //configuring the request
-    const unirest_req = unirest(request.config.method, request.config.url);
-    unirest_req.headers(request.headers);
-    unirest_req.form(request.data);
+// translateRouter.post("/", async (req, res) => {
+//   try {
+//     const body = req.body;
+//     const request = await prepareRequest(body);
+//     //configuring the request
+//     const unirest_req = unirest(request.config.method, request.config.url);
+//     unirest_req.headers(request.headers);
+//     unirest_req.form(request.data);
 
-    //starting the request
-    await unirest_req.end(async (response) => {
-      if (res.error) {
-        console.log(`error happens while unirest performing`);
-        res.send("no result").status(400);
-      } else {
-        res.json(await response.body.data.translations).status(200);
-      }
-    });
-  } catch (error) {
-    console.log(
-      `error happen in the controller while performing unirest request ${error.message}`
-    );
-  }
+//     //starting the request
+//     await unirest_req.end(async (response) => {
+//       if (res.error) {
+//         console.log(`error happens while unirest performing`);
+//         res.send("no result").status(400);
+//       } else {
+//         res.json(await response.body.data.translations).status(200);
+//       }
+//     });
+//   } catch (error) {
+//     console.log(
+//       `error happen in the controller while performing unirest request ${error.message}`
+//     );
+//   }
+// });
+translateRouter.post("/", async (req, res) => {
+  const response = {
+    translate: await translate(req.body, { to: "ar" })
+  };
+  res.json(response).status(200);
 });
 
 module.exports = translateRouter;
